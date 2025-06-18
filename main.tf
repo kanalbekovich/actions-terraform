f62bf1c44411bb9df19c27b5a70f8880450ca90e
kanalbekovich <kanalbekovich@gmail.com>
2025-06-17 20:53:01 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider aws {
+    region = var.region
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+}
+
+variable region {}
+variable ami_id {}
+variable instance_type {}
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-kanatk"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
