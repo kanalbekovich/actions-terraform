73347bbb64edc921f4598ee5cdef325ccd15364c
kanalbekovich <kanalbekovich@gmail.com>
2025-06-18 00:49:42 +0000

    added s3 bucket

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider aws {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-09e6f87a47903347c"
+  instance_type = "t2.micro"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-kanatk"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
