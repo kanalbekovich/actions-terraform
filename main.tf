5a5e82d81458cfa57723727dc3bf92dc6e42f315
kanalbekovich <kanalbekovich@gmail.com>
2025-06-21 12:15:19 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 provider aws {
-    region = "us-east-2"     #var.region
+    region = var.region
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-06971c49acd687c30"  #var.ami_id
-  instance_type = "t2.micro"  #var.instance_type
+  ami           = var.ami_id
+  instance_type = var.instance_type
 }
 
-#variable region {}
-#variable ami_id {}
-#variable instance_type {}
+variable region {}
+variable ami_id {}
+variable instance_type {}
 
 terraform {
   backend "s3" {
