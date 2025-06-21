78c43b6e53ce2a34554469a5ed10f0eb2fac5b02
kanalbekovich <kanalbekovich@gmail.com>
2025-06-21 10:36:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,12 @@
 provider aws {
-    region = var.region
+    region = "us-east-2"     #var.region
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-06971c49acd687c30"  #var.ami_id
+  instance_type = "t2.micro"  #var.instance_type
 }
 
-variable region {}
-variable ami_id {}
-variable instance_type {}
-
-terraform {
-  backend "s3" {
-    bucket = "kaizen-kanatk"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-  }
-}
+#variable region {}
+#variable ami_id {}
+#variable instance_type {}
